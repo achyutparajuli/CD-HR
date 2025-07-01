e8e197f772ac2fa15605003ef0e56d94c1ac1858
Achyut <achyutparajuli49@gmail.com>
2025-07-01 20:10:23 +0545

    push to main

--- resources/views/web/header/header.blade.php
+++ resources/views/web/header/header.blade.php
@@ -2,17 +2,21 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Nirman is a Construction Business HTML Template">
 
-    <title>Nirman - Construction Business HTML Template</title>
-    <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/favicon.png')}}">
+    <meta name="description" content="Find the best manpower recruitment services in Nepal. We help you get jobs abroad with complete documentation, visa processing, and career support.">
+    <meta name="keywords" content="manpower in Nepal, jobs Nepal, foreign jobs Nepal, overseas recruitment Nepal, Gulf jobs Nepal, work abroad Nepal, Nepali workers, manpower agency, job placement Nepal, foreign employment Nepal, Nepal overseas jobs, recruitment agency Nepal">
+    <meta name="author" content="Manpower Recruitment Agency Nepal">
+    <meta name="robots" content="index, follow">
+
+    <title>@yield('title', env('APP_NAME', 'Manpower Recruitment Agency In Nepal'))</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/c-logo.png')}}">
+
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/default.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/blog.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/plugin.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/font-awesome.css') }}" rel="stylesheet" type="text/css">
-
     @stack('css')
 </head>
